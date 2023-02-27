f0110d5931c09e30aeaa5b084d82d9648d232bb5
Franco Ursino <franncoursinno@gmail.com>
2023-02-27 14:53:04 -0300

    finish about section

---
+++ js/skills.js
@@ -0,0 +1,26 @@
+const container = document.querySelector('.about__container--container--container--container');
+
+
+const showContainer = () => {
+  const containerContent = document.createElement('div');
+  containerContent.className = '.about__container--container--container--container--container'
+  
+  containerContent.innerHTML =
+  `
+  <button class="about__container--container--container--container--container--btn" id="softButton">soft Skills</button>
+  <button class="about__container--container--container--container--container--btn" id="hardButton">Hard Skills</button>
+  <p class="about__container--container--container--container--container--text" id="softText">Resolución de problemas | Creativo | Gestión de Personas | Liderazgo | Trabajo en Equipo | Tolerancia a Trabajar Bajo Presión | Escucha Activa | Responsabilidad</p>
+  <p class="about__container--container--container--container--container--text" id="hardText" style="display: none">estos son los hard skills</p>
+  `
+  container.append(containerContent);
+  
+  const hardButton = document.querySelector('#hardButton');
+  const softButton = document.querySelector('#softButton');
+  const hardText = document.querySelector('#hardText');
+  const softText = document.querySelector('#softText');
+
+
+  const changeHard = hardButton.addEventListener('click',() =>{softText.style.display = "none", hardText.style.display = "block"})
+  const changeSoft = softButton.addEventListener('click',() =>{hardText.style.display = "none", softText.style.display = "block"})
+}
+showContainer();
